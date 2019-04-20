3a67143bea1faca3dbd4f0e371dfd99321a72560
İsmail CAN <can@uzem.omu.edu.tr>
2019-04-20 16:23:24 +0300

    Add files via upload

---
+++ setup.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * ÇALIŞAN
+**/
+
+use Grav\Common\Utils;
+
+// Get subsite name from sub-domain
+$environment = isset($_SERVER['HTTP_HOST'])
+    ? $_SERVER['HTTP_HOST']
+    : (isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : 'localhost');
+// Remove port from HTTP_HOST generated $environment
+$environment = strtolower(Utils::substrToString($environment, ':'));
+$folder = "sites/{$environment}";
+
+if ($environment === 'localhost' || !is_dir(ROOT_DIR . "user/{$folder}")) {
+    return [];
+}
+
+return [
+    'environment' => $environment,
+    'streams' => [
+        'schemes' => [
+            'user' => [ 
+				'type' => 'ReadOnlyStream', 
+				'prefixes' => [ 
+					'' => [
+						"user/{$folder}"
+					], 
+				] 
+			], 
+			'cache' => [ 
+				'type' => 'ReadOnlyStream', 
+				'prefixes' => [ 
+					'' => ["cache"], 
+				] 
+			], 
+			'plugins' => [ 
+				'type' => 'ReadOnlyStream', 
+				'prefixes' => [ 
+					'' => [
+						"user/plugins"
+					], 
+				] 
+			], 
+			'themes' => [ 
+				'type' => 'ReadOnlyStream', 
+				'prefixes' => [ 
+					'' => [
+						"user/themes"
+					], 
+				] 
+			], 
+			'images' => [ 
+				'type' => 'ReadOnlyStream', 
+				'prefixes' => [ 
+					'' => [
+						"user/{$folder}/images"
+					], 
+				] 
+			] 
+        ]
+    ]
+];
